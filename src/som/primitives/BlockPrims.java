2642cb6e5f0f31f116d2f365ef12ab64e5f90be4
Dominik Aumayr <daumayr162@gmail.com>
2018-01-11 09:11:59 +0100

    Arbitrary number of Arguments for Blocks

--- src/som/primitives/BlockPrims.java
+++ src/som/primitives/BlockPrims.java
@@ -1,6 +1,5 @@
 package som.primitives;
 
-import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
@@ -16,11 +15,13 @@ import som.VM;
 import som.instrumentation.InstrumentableDirectCallNode.InstrumentableBlockApplyNode;
 import som.interpreter.nodes.DummyParent;
 import som.interpreter.nodes.nary.BinaryExpressionNode;
-import som.interpreter.nodes.nary.QuaternaryExpressionNode;
 import som.interpreter.nodes.nary.TernaryExpressionNode;
 import som.interpreter.nodes.nary.UnaryExpressionNode;
+import som.primitives.arrays.AtPrim;
+import som.primitives.arrays.AtPrimFactory;
 import som.vm.VmSettings;
 import som.vm.constants.KernelObj;
+import som.vmobjects.SArray;
 import som.vmobjects.SBlock;
 import som.vmobjects.SInvokable;
 import tools.dym.Tags.OpClosureApplication;
@@ -163,13 +164,58 @@ public abstract class BlockPrims {
   }
 
   @GenerateNodeFactory
-  public abstract static class ValueMorePrim extends QuaternaryExpressionNode {
-    @Specialization
-    public final Object doSBlock(final SBlock receiver, final Object firstArg,
-        final Object secondArg, final Object thirdArg) {
-      CompilerDirectives.transferToInterpreter();
-      throw new RuntimeException(
-          "This should never be called, because SOM Blocks have max. 2 arguments.");
+  @ImportStatic({BlockPrims.class, SArray.class})
+  @Primitive(primitive = "blockValue:withArguments:", selector = "valueWithArguments:",
+      inParser = false,
+      receiverType = SBlock.class)
+  public abstract static class ValueNPrim extends BinaryExpressionNode {
+
+    @Child SizeAndLengthPrim slprim = SizeAndLengthPrimFactory.create(null);
+    @Child AtPrim            atprim = AtPrimFactory.create(null, null);
+
+    @Override
+    protected boolean isTaggedWithIgnoringEagerness(final Class<?> tag) {
+      if (tag == OpClosureApplication.class) {
+        return true;
+      } else {
+        return super.isTaggedWithIgnoringEagerness(tag);
+      }
+    }
+
+    protected Object[] prepareArguments(final SBlock receiver, final SArray args) {
+      Object[] result = new Object[(int) (slprim.executeEvaluated(args) + 1)];
+      result[0] = receiver;
+      for (int i = 1; i < result.length; i++) {
+        result[i] = atprim.executeEvaluated(null, args, (long) i);
+      }
+      return result;
+    }
+
+    protected long getNumArgs(final SArray args) {
+      return slprim.executeEvaluated(args) + 1;
+    }
+
+    @Specialization(
+        guards = {"cached == receiver.getMethod()",
+            "numArgs == cached.getNumberOfArguments()"},
+        limit = "CHAIN_LENGTH")
+    public final Object doCachedBlock(final SBlock receiver, final SArray args,
+        @Cached("getNumArgs(args)") final long numArgs,
+        @Cached("createDirectCallNode(receiver, getSourceSection())") final DirectCallNode call,
+        @Cached("receiver.getMethod()") final SInvokable cached) {
+      return call.call(prepareArguments(receiver, args));
+    }
+
+    @Specialization(replaces = "doCachedBlock")
+    public final Object doGeneric(final SBlock receiver, final SArray args,
+        @Cached("create()") final IndirectCallNode call) {
+      if (receiver.getMethod().getNumberOfArguments() != getNumArgs(args)) {
+
+        return KernelObj.signalException("signalBAMismatch:actual:",
+            (long) receiver.getMethod().getNumberOfArguments() - 1,
+            slprim.executeEvaluated(args));
+      }
+      return receiver.getMethod().invoke(call, prepareArguments(receiver, args));
     }
   }
 }
